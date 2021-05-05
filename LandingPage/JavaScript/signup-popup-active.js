5eeeadc167dfdfd96dce09f86e65851d26a336c7
nicholascarlin@wustl.edu <nicholascarlin@wustl.edu>
2021-05-04 20:51:38 -0500

    Added password email focusing and carrying text. Changed button border radius

--- LandingPage/JavaScript/signup-popup-active.js
+++ LandingPage/JavaScript/signup-popup-active.js
@@ -8,11 +8,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
             signupPopup[i].classList.add("sup-active");
         }
 
+        document.getElementById('email-signup-input').value = document.getElementById('first-signup-input').value;
+
         let pg = document.getElementById('pg');
 
         pg.style.visibility = "visible";
         pg.style.opacity = .6;
 
+        document.getElementById("password-signup-input").value = "";
+
+        console.log(document.getElementById('first-signup-input').value);
+
+        if(document.getElementById('first-signup-input').value == "") {
+            document.getElementById('email-signup-input').focus()
+        } else {
+            document.getElementById("password-signup-input").focus();
+        }
     })
 
     document.getElementById('close-button').addEventListener("click", function() {
