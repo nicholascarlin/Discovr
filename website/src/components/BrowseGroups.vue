020b00321f5b4fefe9aabd6f83682f56ae75b2ab
nicholascarlin <nicholascarlin@wustl.edu>
2021-05-19 14:35:29 -0500

    Some dynamic ass shit

--- website/src/components/BrowseGroups.vue
+++ website/src/components/BrowseGroups.vue
@@ -1,27 +1,69 @@
 <template>
     <div class="browse-groups">
-        <div class="your-groups-subheader"> Your Groups</div>
+        <div class="your-groups-subheader">Your Groups</div>
+        <div class="group-icon-scroll-window">
+            <li class="group-icon-list" v-for="group in Groups" :key="group.groupName">
+                <dynamic-group-icons :groupName="group.groupName"></dynamic-group-icons>
+            </li>
+        </div>
         <div class="dynamic-groups">
-            <div></div>
         </div>
     </div>
 </template>
 
 <script>
+import DynamicGroupIcons from './DynamicGroupIcons.vue';
+
 export default {
-    
-}
+    components: {
+       DynamicGroupIcons
+    },
+    data() {
+        return {
+            Groups: [
+                { groupName: "Guitar" },
+                { groupName: "Banjo" },
+                { groupName: "Dobro" },
+                { groupName: "Bass" },
+                { groupName: "Drums" },
+                { groupName: "Bass" },
+                { groupName: "Drums" }
+            ]
+        };
+    } 
+};
 </script>
 
 <style>
+
+    .group-icon-scroll-window {
+        clear: both;
+        width: 65%;
+        height: 100px;
+        overflow-x: auto;
+        overflow-y: hidden;
+        white-space: nowrap;
+        margin-left: 50px;
+    }
 
     .browse-groups {
         margin: 30px 0px 0px 15px;
     }
 
+    .group-icon-list {
+        margin-top: -20px;
+        text-align: center;
+        display: inline-block;
+        list-style: none;
+    }
+
     .your-groups-subheader {
         float: left;
         font-size: 20px;
+    }
+
+    .dg-icons {
+        float: left;
     }
 
 </style>
---
+++ website/src/components/DynamicGroupIcons.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="group-icon">{{groupName}}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        groupName: {required: true, type: String},
+    }
+}
+</script>
+
+<style>
+    .group-icon {
+        float: left;
+        display: inline-block;
+        text-align: center;
+        width: 150px;
+        margin-top: 40px;
+        margin-left: 10px;
+        margin-right: 10px;
+        font-size: 20px;
+        padding-top: 15px;
+        padding-bottom: 15px;
+        background-color: lightblue;
+        border-radius: 20px;
+    }
+
+</style>
