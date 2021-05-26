2cf63b8a4c03d17cc3262eb2c4d39abbd9016999
nicholascarlin <nicholascarlin@wustl.edu>
2021-05-26 12:35:36 -0600

    Got url and title auto pulled

--- ChromeExtension/Scripts/grabURLAndTitle.js
+++ ChromeExtension/Scripts/grabURLAndTitle.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        console.log(tabs[0].url);
+        document.getElementById('title-input').value = tabs[0].title;
         document.getElementById("link-input").value = tabs[0].url;
     });
 
