5d61c7e0dc5e757a233b89a93e1f2da0cfcd817a
nicholascarlin <nicholascarlin@wustl.edu>
2021-05-26 14:46:47 -0600

    Improved star rating functionality with clearing

--- ChromeExtension/Scripts/ratings.js
+++ ChromeExtension/Scripts/ratings.js
@@ -6,24 +6,51 @@ window.addEventListener('DOMContentLoaded', (event) => {
     star4 = document.getElementById("star4");
     star5 = document.getElementById("star5");
 
+    index = 0;
+
     star1.addEventListener("click", function() {
-        setStars(1);
+        if(index == 1) {
+            clearStars();
+        } else {
+            index = 1;
+            setStars(1);
+        }
     });
 
     star2.addEventListener("click", function() {
-        setStars(2);
+        if(index == 2) {
+            clearStars();
+        } else {
+            index = 2;
+            setStars(2);
+        }
     });
 
     star3.addEventListener("click", function() {
-        setStars(3);
+        if(index == 3) {
+            clearStars();
+        } else {
+            index = 3;
+            setStars(3);
+        }
     });
 
     star4.addEventListener("click", function() {
-        setStars(4);
+        if(index == 4) {
+            clearStars();
+        } else {
+            index = 4;
+            setStars(4);
+        }
     });
 
     star5.addEventListener("click", function() {
-        setStars(5);
+        if(index == 5) {
+            clearStars();
+        } else {
+            index = 5;
+            setStars(5);
+        }
     });
 
 
@@ -40,4 +67,14 @@ function setStars(index) {
         star = document.getElementById(`star${i}`);
         star.style.background = "#b9b9b9";
     }
+}
+
+function clearStars() {
+
+    for(i = 1; i <= 5; i++) {
+        star = document.getElementById(`star${i}`);
+        star.style.background = "#b9b9b9";
+    }
+
+    index = 0;
 }
