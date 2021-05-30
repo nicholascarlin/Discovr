7d2fd0471ff1fabe1871e843ce05eae3d0e00345
nicholascarlin <nicholascarlin@wustl.edu>
2021-05-29 19:09:36 -0600

    Type but still broken

---
+++ ChromeExtension/Scripts/type.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    
+    let input = document.getElementById("type-input");
+
+    input.addEventListener("input", function(e){
+    
+        var isInputEvent = (Object.prototype.toString.call(e).indexOf("InputEvent") > -1);
+        
+        if(!isInputEvent)
+            console.log("Selected: " + e.target.value);
+            AddElement(e.target.value);
+        }, false);
+});
+
+function AddElement(text) {
+    let menu = document.getElementById("type-menu");
+
+    let entry = document.createElement('li');
+    let span = document.createElement('span');
+
+    span.setAttribute('class', 'close');
+    span.innerHTML = "x";
+
+    entry.innerHTML = text;
+    entry.appendChild(span);
+    entry.classList.add("type");
+
+    menu.appendChild(entry);
+}
